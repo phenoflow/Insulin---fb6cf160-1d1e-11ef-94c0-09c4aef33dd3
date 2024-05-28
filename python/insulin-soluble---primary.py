da79038ea4cbfd7a308d4e3ae99e4c387d5f5186
Martin Chapman <contact@martinchapman.co.uk>
2024-05-28 19:24:27 +0100

    Update made by Phenoflow

---
+++ python/insulin-soluble---primary.py
@@ -0,0 +1,25 @@
+# Matthew J Carr, Alison K Wright, Lalantha Leelarantha, Hood Thabit, Nicola Milne, Naresh Kanumilli, Darren M Ashcroft, Martin K Rutter, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"26621","system":"gprdproduct"},{"code":"16209","system":"gprdproduct"},{"code":"20196","system":"gprdproduct"},{"code":"18301","system":"gprdproduct"},{"code":"63679","system":"gprdproduct"},{"code":"9079","system":"gprdproduct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('insulin-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["insulin-soluble---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["insulin-soluble---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["insulin-soluble---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
